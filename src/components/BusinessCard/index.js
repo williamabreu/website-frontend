e529c6a1f0857cf97eaa340dde5898378ed59bb5
William Abreu <william@t10.digital>
2023-03-22 15:17:15 -0300

    Updated social networks

--- src/components/BusinessCard/index.js
+++ src/components/BusinessCard/index.js
@@ -1,7 +1,5 @@
 import React from "react"
 import { Button, ButtonGroup, Col, Container, Row, Image } from "react-bootstrap"
-import Tooltip from "react-bootstrap/Tooltip"
-import OverlayTrigger from "react-bootstrap/OverlayTrigger"
 
 const BusinessCard = (props) => (
   <Container className="business-card px-4 py-5">
@@ -32,19 +30,9 @@ const BusinessCard = (props) => (
     {props.socialNetworks &&
       <ButtonGroup>
         {props.socialNetworks.map(item => (
-          <OverlayTrigger
-            placement="bottom"
-            delay={{ show: 250, hide: 200 }}
-            overlay={
-              <Tooltip>
-                {item.name}
-              </Tooltip>
-            }
-          >
-            <Button variant="outline-dark" href={item.link} target="_blank" >
-              {item.icon}
-            </Button>
-          </OverlayTrigger>
+          <Button variant="outline-dark" href={item.link} target="_blank" >
+            {item.icon} {item.name}
+          </Button>
         ))}
       </ButtonGroup>
     }
